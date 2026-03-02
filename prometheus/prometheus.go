1c5d47405a03d33b6c655d96410e8872b76d2a0f
joil <joil-z@outlook.com>
2026-03-02 21:45:55 +0800

    feat: 实现prometheus插件

---
+++ prometheus/prometheus.go
@@ -0,0 +1,116 @@
+package prometheus
+
+import (
+	"time"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"gorm.io/gorm"
+)
+
+type DurationCallbacks struct {
+	vector *prometheus.SummaryVec
+}
+
+func NewDurationCallbacks(opts prometheus.SummaryOpts) *DurationCallbacks {
+	vector := prometheus.NewSummaryVec(opts,
+		[]string{"type", "table"})
+	prometheus.MustRegister(vector)
+	return &DurationCallbacks{
+		vector: vector,
+	}
+}
+
+func (c *DurationCallbacks) Name() string {
+	return "prometheus"
+}
+
+func (c *DurationCallbacks) Initialize(db *gorm.DB) error {
+	err := db.Callback().Create().Before("*").
+		Register("prometheus_create_before", c.Before())
+	if err != nil {
+		return err
+	}
+
+	err = db.Callback().Create().After("*").
+		Register("prometheus_create_after", c.After("CREATE"))
+	if err != nil {
+		return err
+	}
+
+	err = db.Callback().Query().Before("*").
+		Register("prometheus_query_before", c.Before())
+	if err != nil {
+		return err
+	}
+
+	err = db.Callback().Query().After("*").
+		Register("prometheus_query_after", c.After("QUERY"))
+	if err != nil {
+		return err
+	}
+
+	err = db.Callback().Raw().Before("*").
+		Register("prometheus_raw_before", c.Before())
+	if err != nil {
+		return err
+	}
+
+	err = db.Callback().Raw().After("*").
+		Register("prometheus_raw_after", c.After("RAW"))
+	if err != nil {
+		return err
+	}
+
+	err = db.Callback().Update().Before("*").
+		Register("prometheus_update_before", c.Before())
+	if err != nil {
+		return err
+	}
+
+	err = db.Callback().Update().After("*").
+		Register("prometheus_update_after", c.After("UPDATE"))
+	if err != nil {
+		return err
+	}
+
+	err = db.Callback().Delete().Before("*").
+		Register("prometheus_delete_before", c.Before())
+	if err != nil {
+		return err
+	}
+
+	err = db.Callback().Delete().After("*").
+		Register("prometheus_delete_after", c.After("DELETE"))
+	if err != nil {
+		return err
+	}
+
+	err = db.Callback().Row().Before("*").
+		Register("prometheus_row_before", c.Before())
+	if err != nil {
+		return err
+	}
+
+	err = db.Callback().Row().After("*").
+		Register("prometheus_row_after", c.After("ROW"))
+	return err
+}
+
+func (c *DurationCallbacks) Before() func(db *gorm.DB) {
+	return func(db *gorm.DB) {
+		start := time.Now()
+		db.Set("start_time", start)
+	}
+}
+
+func (c *DurationCallbacks) After(typ string) func(db *gorm.DB) {
+	return func(db *gorm.DB) {
+		val, _ := db.Get("start_time")
+		start, ok := val.(time.Time)
+		if ok {
+			duration := time.Since(start).Milliseconds()
+			c.vector.WithLabelValues(typ, db.Statement.Table).
+				Observe(float64(duration))
+		}
+	}
+}
